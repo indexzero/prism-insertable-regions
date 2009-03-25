0b912938cb5a26930bc35b9ce3767ad3849ee6c7
Charlie Robbins <charlie.robbins@gmail.com>
2009-03-25 17:57:27 -0400

    Initial commit of support for IRegion.Insert to GitHub

---
+++ README.txt
@@ -0,0 +1,7 @@
+Author: Charlie Robbins, 2008
+
+Last Update: Prism v2 Final
+
+This extension of Prism allows for IRegion.Insert to be meaningful. You also need to break the Project Links to these files since there are not matching modified versions for the WPF Composite Application Library. The location of the source files in this folder match their directories in the Prism source code from /CAL. Simply replace the files then re-build the binaries and you will be able to call IRegion.Insert.
+
+If you have any feedback, please email me. 
---
+++ Silverlight/Composite.Presentation/Regions/ViewsCollection.Silverlight.cs
@@ -0,0 +1,45 @@
+//===================================================================================
+// Microsoft patterns & practices
+// Composite Application Guidance for Windows Presentation Foundation and Silverlight
+//===================================================================================
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===================================================================================
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//===================================================================================
+namespace Microsoft.Practices.Composite.Presentation.Regions
+{
+    using System.Collections;
+    using System.Collections.Specialized;
+    using System.Collections.Generic;
+
+    public partial class ViewsCollection
+    {
+        private void AddAndNotify(object item)
+        {
+            int index = filteredCollection.Count;
+            filteredCollection.Add(item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+        }
+
+        private void InsertAndNotify(object item, int index)
+        {
+            filteredCollection.Insert(index, item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+        }
+
+        private void RemoveAndNotify(object item)
+        {
+            int index = filteredCollection.IndexOf(item);
+            filteredCollection.Remove(item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index));
+        }
+    }
+}
